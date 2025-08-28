7ab490db26b7b7969736d5b62711aa05bb2f9caa
Tyler Stuart <tylerstuart11@gmail.com>
2025-08-28 09:50:05 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Tyler Stuart
+ *         tstuart6@huskers.unl.edu
+ * Date: 2026/08/28
+ *
+ *
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Tyler Stuart\n");
+
+  return 0;
+}
